139ee15b96e0ae0bb464ff4eb27559afd9f13659
João Maia <15joaomaia@gmail.com>
2022-02-25 11:00:37 -0300

    projeto finalizado como parte do desafio theme switcher do discover da rocketseat

--- javascript/toggle.js
+++ javascript/toggle.js
@@ -4,6 +4,18 @@ let body = document.getElementsByTagName('body')
 let icon = document.getElementsByTagName('i')
 let button = document.getElementsByTagName('button')
 
+let code = localStorage.getItem('code')
+
+// console.log(typeof(code))
+
+if(code == 'true'){
+    body[0].classList.add('is-dark') 
+    // body[0].classList.remove('is-light')
+    button[0].classList.toggle('is-light')
+    icon[0].classList.toggle('fa-sun')
+    // icon[0].classList.remove('fa-moon')
+}
+
 toggle.addEventListener('click', (e)=>{
     body[0].classList.toggle('is-dark')
     body[0].classList.remove('is-light')
@@ -13,9 +25,7 @@ toggle.addEventListener('click', (e)=>{
     
     if (e.target.classList[4] == 'is-light' || icon[0].classList[2] == 'fa-sun'){
         localStorage.setItem('code', true)
-        console.log('true')
     }else{ 
-        console.log('false')
         localStorage.setItem('code', false)
     }
 })
